cab000d2948b33a2bb0427d5f40f081e6b19e0ea
Hien Le <hienhydra11@gmail.com>
2021-01-08 11:42:47 +0700

    ADD communication with Arduino

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import cv2 as cv
 import os
 import socket
 import base64
+import serial
 import threading
 import ObjectDetection
 from multiprocessing import Process
@@ -10,14 +11,15 @@ import time
 # socket_io = socketio.Client()
 # socket_io.connect('http://192.168.1.5:8000')
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-control_station_address = ('192.168.1.5', 2711)
+control_station_address = ('192.168.137.57', 2711)
 
 # Create new socket to listen to key press from JavaFX
 sock2 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-pi_address = ('192.168.1.135', 2345)
+pi_address = ('192.168.137.254', 2345)
+# Bind socket2 to server
+sock2.bind(pi_address)
+
 def test_openCV():
-    # Bind socket2 to server
-    sock2.bind(pi_address)
 
     cap = cv.VideoCapture(0)
     # cap.set(3, 480)
@@ -35,8 +37,8 @@ def test_openCV():
             break
         if cv.waitKey(1) == ord('q'):
             break
-        frame_contours = ObjectDetection.getContours(frame)
-        reval, buffer = cv.imencode('.jpeg', frame_contours)
+        # frame_contours = ObjectDetection.getContours(frame)
+        reval, buffer = cv.imencode('.jpeg', frame)
         encoded_string = base64.b64encode(buffer)
         # image = encoded_string.decode('utf-8')
         # socket_io.emit('image', image)
@@ -58,25 +60,16 @@ def test_openCV():
     cv.destroyAllWindows()
 
 
-class MyThread (threading.Thread):
-    def __init__(self, thread_id, name):
-        threading.Thread.__init__(self)
-        self.thread_id = thread_id
-        self.name = name
-
-    def run(self):
-        print("Starting " + self.name)
-        # Listen for key press
-        while True:
-            data, address = sock2.recvfrom(1024)
-            print(address, ": ", data.decode())
-
 def receiveCommands():
     # Listen for key press
     print("start new process")
+    ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
+    ser.flush()
     while True:
         data, address = sock2.recvfrom(1024)
-        print(address, ": ", data.decode())
+        print(data.decode())
+        ser.write(data)
+        time.sleep(0.1)
 
 def main():
     # thread1 = MyThread(1, "Thread-1")
@@ -84,7 +77,7 @@ def main():
     # print("here")
     p = Process(target=receiveCommands, args=(''))
     p.start()
-    test_openCV()
+    # test_openCV()
     p.join()
 
 if __name__ == "__main__":
